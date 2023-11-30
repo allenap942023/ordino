7b99b9ecf31975ad3b4926cd2fdcde1e3d36e281
undefined <undefined>
2023-11-29 23:53:32 -0600

    🐛`Fix on centered patient info

--- front-end/src/components/HistorialMedico.jsx
+++ front-end/src/components/HistorialMedico.jsx
@@ -8,14 +8,14 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 import DatosPaciente from "./DatosPaciente";
 const HistorialMedico = () => {
   let { id } = useParams();
-  const URLBackEnd = 'http://localhost:3000/api';
+  const URLBackEnd = "http://localhost:3000/api";
   const [historial, setHistorial] = useState([]);
 
   useEffect(() => {
     obtenerHistorial();
-  }, [])
+  }, []);
   const obtenerHistorial = (event) => {
-    var token = window.localStorage.getItem('token');
+    var token = window.localStorage.getItem("token");
     var fechaFin = null;
     var fechaInicio = null;
     if (event) {
@@ -23,21 +23,25 @@ const HistorialMedico = () => {
       fechaInicio = event.target.fechaInicio.value;
       fechaFin = event.target.fechaFin.value;
     }
-    axios.get(`${URLBackEnd}/pacientes/${id}/historial?fechaFin=${fechaFin}&fechaInicio=${fechaInicio}`, {
-      headers: {
-        Authorization: `Bearer  ${token}`
-      }
-    }).then(
-      (res) => {
+    axios
+      .get(
+        `${URLBackEnd}/pacientes/${id}/historial?fechaFin=${fechaFin}&fechaInicio=${fechaInicio}`,
+        {
+          headers: {
+            Authorization: `Bearer  ${token}`,
+          },
+        }
+      )
+      .then((res) => {
         var datosRecibidos = res.data;
         console.log(datosRecibidos);
         setHistorial(datosRecibidos);
         console.log(historial);
-      }
-    ).catch((error) => {
-      console.log(error);
-    });
-  }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const [selectedOption, setSelectedOption] = useState(null);
   const [listaPacientes, setListaPacientes] = useState([]);
@@ -46,8 +50,6 @@ const HistorialMedico = () => {
   const handleOptionClick = (option) => {
     setSelectedOption(option);
   };
-
-
 
   return (
     <div className="flex h-screen ">
@@ -60,21 +62,22 @@ const HistorialMedico = () => {
         id="Principal"
         className="flex-1 flex flex-col overflow-auto items-start rounded-lg sm:ml-[180px] font-inter"
       >
-         <DatosPaciente id_paciente={id} />
+        <div className="flex justify-center w-full">
+          <DatosPaciente id_paciente={id} />
+        </div>
         <div className="flex items-center mx-auto w-5/6 mt-10">
           <FaNotesMedical className="mr-2" size={20} />{" "}
           {/* Icono de expedientes médicos */}
-          <p className=" text-sm">
-            Listado de consultas:
-          </p>
+          <p className=" text-sm">Listado de consultas:</p>
         </div>
 
         <div className="flex flex-col items-center mx-auto w-5/6">
-          <form className="w-full mx-auto flex flex-row items-center" onSubmit={obtenerHistorial}>
+          <form
+            className="w-full mx-auto flex flex-row items-center"
+            onSubmit={obtenerHistorial}
+          >
             <div className="w-1/2 mx-auto flex items-center bg-white rounded-xl border border-gray-300 p-2 m-5">
-              <div className="mr-4">
-
-              </div>
+              <div className="mr-4"></div>
 
               <input
                 type="date"
@@ -84,9 +87,7 @@ const HistorialMedico = () => {
               />
             </div>
             <div className="w-1/2 mx-auto flex items-center bg-white rounded-xl border border-gray-300 p-2 m-5">
-              <div className="mr-4">
-
-              </div>
+              <div className="mr-4"></div>
 
               <input
                 type="date"
@@ -95,33 +96,35 @@ const HistorialMedico = () => {
                 className="flex-1 px-2 text-xs outline-none bg-transparent"
               />
             </div>
-            <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white w-[100px] h-[40px] flex items-center justify-center rounded-2xl"> <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 text-white"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-600 text-white w-[100px] h-[40px] flex items-center justify-center rounded-2xl"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-5.2-5.2M15 10a5 5 0 11-10 0 5 5 0 0110 0z"
-              />
-            </svg></button>
+              {" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4 text-white"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-5.2-5.2M15 10a5 5 0 11-10 0 5 5 0 0110 0z"
+                />
+              </svg>
+            </button>
           </form>
           {historial.length > 0 ? (
             <div className="overflow-x-auto w-full m-5">
-              <h2 className="text-sm">
-                Coincidencias encontradas
-              </h2>
+              <h2 className="text-sm">Coincidencias encontradas</h2>
 
               <div className="mt-5">
                 <div className="rounded-xl overflow-x-auto shadow-md border border-slate-800">
-
                   <table className="w-full shadow-md rounded-md text-xs overflow-x-auto">
                     <thead className="bg-slate-800 text-neutral-100 text-xs">
-
                       <tr>
                         {/* <th className="py-3 px-6 text-left lg:w-1/6 border-r-2 border-r-slate-700">
         Nombre
@@ -149,45 +152,48 @@ const HistorialMedico = () => {
                     <tbody className="text-neutral-800">
                       {/* Ejemplo de datos de citas */}
                       {historial.map((item, index) => {
-                        return <>
-                          <tr className="bg-white hover:bg-slate-100">
-                            {/* <td className="py-4 px-6 lg:w-1/6 text-xs border-r-2">
+                        return (
+                          <>
+                            <tr className="bg-white hover:bg-slate-100">
+                              {/* <td className="py-4 px-6 lg:w-1/6 text-xs border-r-2">
         Juan Pérez
       </td> */}
-                            <td className="py-4 px-6 lg:w-1/6 text-xs border-r-2">
-                              {index+1}
-                            </td>
-                            <td className="py-4 px-6 lg:w-1/12 text-center border-r-2">
-                              {moment(item.fecha_cita).format('DD')}
-                            </td>
-                            <td className="py-4 px-6 lg:w-1/12 text-center border-r-2">
-                              {moment(item.fecha_cita).format('MM')}
-                            </td>
-                            <td className="py-4 px-6 lg:w-1/12 text-center border-r-2">
-                              {moment(item.fecha_cita).year()}
-                            </td>
-                            <td className="py-4 px-6 lg:w-1/4 text-center border-r-2">
-                              {item.diagnostico}
-                            </td>
-                            <td className="py-4 px-6 lg:w-1/6 text-center">
-                              <a
-                              href={"/dashboard/consultation/"+id+"/"+item._id}
-                                className="text-blue-500 py-2 px-4 rounded-md transition duration-300"
-                              >
-                                <FaExternalLinkAlt className="mr-2"></FaExternalLinkAlt>
-                                Ver
-                              </a>
-                            </td>
-                          </tr>
-                        </>
-                      })
-                      }
+                              <td className="py-4 px-6 lg:w-1/6 text-xs border-r-2">
+                                {index + 1}
+                              </td>
+                              <td className="py-4 px-6 lg:w-1/12 text-center border-r-2">
+                                {moment(item.fecha_cita).format("DD")}
+                              </td>
+                              <td className="py-4 px-6 lg:w-1/12 text-center border-r-2">
+                                {moment(item.fecha_cita).format("MM")}
+                              </td>
+                              <td className="py-4 px-6 lg:w-1/12 text-center border-r-2">
+                                {moment(item.fecha_cita).year()}
+                              </td>
+                              <td className="py-4 px-6 lg:w-1/4 text-center border-r-2">
+                                {item.diagnostico}
+                              </td>
+                              <td className="py-4 px-6 lg:w-1/6 text-center">
+                                <a
+                                  href={
+                                    "/dashboard/consultation/" +
+                                    id +
+                                    "/" +
+                                    item._id
+                                  }
+                                  className="text-blue-500 py-2 px-4 rounded-md transition duration-300"
+                                >
+                                  <FaExternalLinkAlt className="mr-2"></FaExternalLinkAlt>
+                                  Ver
+                                </a>
+                              </td>
+                            </tr>
+                          </>
+                        );
+                      })}
                       {/* Más filas de citas aquí */}
                     </tbody>
                   </table>
-
-
-
                 </div>
               </div>
             </div>
@@ -203,4 +209,3 @@ const HistorialMedico = () => {
 };
 
 export default HistorialMedico;
-
