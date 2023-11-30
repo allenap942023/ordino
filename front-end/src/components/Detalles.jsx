5fbe1a839df90feb4de4fb58d379aa94977aa001
allenwalker2015 <00053812@uca.edu.sv>
2023-11-30 00:08:49 -0600

    Se ha quitado el boton de enviar

--- front-end/src/components/Detalles.jsx
+++ front-end/src/components/Detalles.jsx
@@ -208,9 +208,9 @@ const Detalles = () => {
                   value={formData.contacto_emergencia&& formData.contacto_emergencia.correo_contacto}></textarea>
       </div>
 
-      <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+      {/* <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
         Enviar
-      </button>
+      </button> */}
     </form>
     </>
   );
