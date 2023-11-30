f5df53b38090ef7ea5c7ae5e9a5724d2383337c2
undefined <undefined>
2023-11-29 23:00:31 -0600

    ♻️ Refactor on patient data

--- front-end/src/components/DatosPaciente.jsx
+++ front-end/src/components/DatosPaciente.jsx
@@ -1,69 +1,60 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 const URLBackEnd = "http://localhost:3000/api";
 
 const DatosPaciente = (props) => {
+  const [paciente, setPaciente] = useState({});
+  useEffect(() => {
+    obtenerInfoUsuario();
+  }, [paciente]);
 
-    const [paciente, setPaciente] = useState({});
-    useEffect(() => {
-        obtenerInfoUsuario();
-    }, [paciente]);
+  const obtenerInfoUsuario = () => {
+    var id = props.id_paciente;
+    var token = window.localStorage.getItem("token");
 
-    const obtenerInfoUsuario = () => {
-        var id = props.id_paciente;
-        var token = window.localStorage.getItem("token");
+    axios
+      .get(`${URLBackEnd}/pacientes/${id}`, {
+        headers: {
+          Authorization: `Bearer  ${token}`,
+        },
+      })
+      .then((res) => {
+        var datosRecibidos = res.data;
+        console.log(datosRecibidos);
+        setPaciente(datosRecibidos);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-        axios
-            .get(`${URLBackEnd}/pacientes/${id}`, {
-                headers: {
-                    Authorization: `Bearer  ${token}`,
-                },
-            })
-            .then((res) => {
-                var datosRecibidos = res.data;
-                console.log(datosRecibidos);
-                setPaciente(datosRecibidos);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    };
-
-
-    return (
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-1 pb-4 w-full">
-            
-            <div className="flex flex-row">
-                <span>Nombre paciente</span>
-                <span>{paciente.nombre}</span>
-            </div>
-            <div className="flex flex-row">
-                <span>Fecha de nacimiento</span>
-                <span>{paciente.fecha_nacimiento}</span>
-            </div>
-            <div className="flex flex-row">
-                <span>Dui</span>
-                <span>{paciente.dui_paciente}</span>
-            </div>
-            <div className="flex flex-row">
-                <span>Sexo</span>
-                <span>{paciente.sexo_paciente}</span>
-            </div>
-            <div className="flex flex-row">
-                <span>Direccion</span>
-                <span>{paciente.direccion}</span>
-            </div>
-            <div className="flex flex-row">
-                <span>Numero de telefono</span>
-                <span>{paciente.numero_telefonico}</span>
-            </div>
-            <div className="flex flex-row">
-                <span>Correo electrónico</span>
-                <span>{paciente.correo}</span>
-            </div>
-
-        </div>
-    );
+  return (
+    <div className="flex justify-evenly shadow-md p-4 rounded-3xl m-3 min-w-[500px] lg:min-w-[900px] font-inter">
+      <div className="lg:w-[300px] flex justify-start">
+        <img
+          src=""
+          alt=""
+          className="rounded-full border border-slate-800 w-[120px] h-[120px] shadow-md"
+        />
+      </div>
+      <div className="grid grid-cols-2 lg:grid-cols-4">
+        <span className="font-bold text-xs">Nombre paciente:</span>
+        <span className="text-xs">{paciente.nombre}</span>
+        <span className="font-bold text-xs">Fecha de nacimiento:</span>
+        <span className="text-xs">{paciente.fecha_nacimiento}</span>
+        <span className="font-bold text-xs">Sexo:</span>
+        <span className="text-xs">{paciente.sexo_paciente}</span>
+        <span className="font-bold text-xs">Dui</span>
+        <span className="text-xs">{paciente.dui_paciente}</span>
+        <span className="font-bold text-xs">Direccion</span>
+        <span className="text-xs">{paciente.direccion}</span>
+        <span className="font-bold text-xs">Numero de telefono</span>
+        <span className="text-xs">{paciente.numero_telefonico}</span>
+        <span className="font-bold text-xs">Correo electrónico</span>
+        <span className="text-xs">{paciente.correo}</span>
+      </div>
+    </div>
+  );
 };
 
 export default DatosPaciente;
